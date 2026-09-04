c1f54a27766edc9e398f99013f00adc72d74f4fd
hiverkiya <hiverkiya@gmail.com>
2026-09-04 16:57:50 +0530

    feat: integrate Sentry error tracking and performance monitoring into the Next.js application

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs"
+
+Sentry.init({
+  dsn: "https://6321d19ff0f2112f03777bcdfac6a07e@o4510577748213760.ingest.us.sentry.io/4512027777302528",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  dataCollection: {
+    // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#dataCollection
+    // userInfo: false,
+    // httpBodies: [],
+  },
+})
